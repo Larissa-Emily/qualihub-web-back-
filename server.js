a0571da9c247811cdd9f10536313de90fe36d6bf
Larissa Emily <larissaemily456@gmail.com>
2025-12-30 11:24:34 -0300

    update port

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: process.env.SMTP_PORT,
-  secure: true,
+  secure: false,
   auth: { 
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
