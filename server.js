46a71ec419a8abb572c7990a7bb08ec6710253e0
Larissa Emily <larissaemily456@gmail.com>
2025-12-30 09:33:56 -0300

    Deploy backend

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.use(express.json());
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: process.env.SMTP_PORT,
-  secure: false,
-  auth: {
+  secure: true,
+  auth: { 
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
